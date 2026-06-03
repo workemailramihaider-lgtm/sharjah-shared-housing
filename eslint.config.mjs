cd9c4623388ed7280615b78e2cdfbda8fe85a88e
workemailramihaider-lgtm <workemailramihaider@gmail.com>
2026-06-04 03:02:46 +0400

    Refactor ESLint config to use FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,16 @@
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...nextVitals
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
